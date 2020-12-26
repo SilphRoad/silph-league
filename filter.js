b8d2445e6d12bacf5a836819569ccdc18c20ecf1
Katlyn Lorimer <katlyn@katlyn.dev>
2020-12-26 15:30:01 -0700

    Allow League assets to show on any year

--- filter.js
+++ filter.js
@@ -24,7 +24,7 @@ const updateFilter = (type = typeSelection, date = dateSelection) => {
   assets.forEach(asset => {
     if (typeSelection === 'all' || asset.dataset.type === typeSelection) {
       // We want to make sure that assets without a year are let through
-      if (asset.dataset.year === dateSelection || (typeSelection === 'league' && asset.dataset.type === typeSelection)) {
+      if (asset.dataset.year === dateSelection || asset.dataset.type === 'league') {
         asset.classList.add('show')
       } else {
         asset.classList.remove('show')
